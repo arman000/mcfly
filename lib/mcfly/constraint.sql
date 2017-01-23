52b9542684ab981d7cfb6911390e76e75ea17dd2
Arman Bostani <arman.bostani@pnmac.com>
2017-01-23 14:12:00 -0800

    Relaxed new obsoledted_dt constraint to obsoleted_dt >= created_dt -- there were lost of example in our applications of obsoleted_dt=created_dt.

--- lib/mcfly/constraint.sql
+++ lib/mcfly/constraint.sql
@@ -3,5 +3,7 @@
 ALTER TABLE "%{table}" ADD CONSTRAINT check_o_user
 CHECK ((obsoleted_dt = 'Infinity') = (o_user_id IS NULL));
 
+-- obsoleted_dt > created_dt should be enforced -- however, there are
+-- many instances of obsoleted_dt=created_dt in our existing apps.
 ALTER TABLE "%{table}" ADD CONSTRAINT check_bad_obsolete
-CHECK (obsoleted_dt > created_dt);
+CHECK (obsoleted_dt >= created_dt);
